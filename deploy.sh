47404a7d86d38b009391772a01ee37a92d974fbd
megiakhalkatsi <megi.akhalkatsi@gmail.com>
2020-04-20 22:07:22 +0400

    vue deploy files

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
  git push -f https://github.com/megiakhalkatsi/vue-skills.git master:gh-pages
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // vue.config.js
 module.exports = {
     // options...
+    
     // publicPath: '/vue-skills/'
     publicPath: process.env.NODE_ENV === 'production' ? '/vue-skills/' : '/'
   }
